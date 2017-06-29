c64b5ea177f30583b3570836f30ef7f79a64e21c
rubonics <rubenrodriguez+github@gmail.com>
2017-06-28 21:35:13 -0400

    better server configuration
    + allows for the incoming server paramter to include a protocol or not

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = RMMigrate;
 
-function RMMigrate (backup, options) {
-  return require('webhook-migrate')(backup, options)
+function RMMigrate (backup, options, callback) {
+  return require('webhook-migrate')(backup, options, callback )
 }
